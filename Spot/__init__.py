e7606b4e0b657a22404031321c97b0c2b6b172ae
Macsback <119421402+Macsback@users.noreply.github.com>
2025-01-06 17:18:02 +0000

    Connecting information from Database.

--- Spot/__init__.py
+++ Spot/__init__.py
@@ -3,16 +3,15 @@ from .mydb import db
 from .routes import register_routes
 
 def create_app():
-    """Create and configure the Flask app."""
     app = Flask(__name__)
     app.secret_key = "spot_secret_key"
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/spot_db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Initialize the database
+ 
     db.init_app(app)
 
-    # Register routes
+
     register_routes(app)
 
     return app
